6741c38139f8d3ea44b0c665b0566887bfac04c8
Koinos <67607993+koinos-ci@users.noreply.github.com>
2021-09-02 21:06:07 +0000

    Update for koinos-proto commit 6cc4a98

---
+++ koinos/protocol/system_call_ids.js
@@ -0,0 +1,53 @@
+// source: koinos/protocol/system_call_ids.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.koinos.protocol.system_call_id');
+
+/**
+ * @enum {number}
+ */
+proto.koinos.protocol.system_call_id = {
+  RESERVED_ID: 0,
+  PRINTS: 1,
+  VERIFY_BLOCK_HEADER: 2,
+  APPLY_BLOCK: 3,
+  APPLY_TRANSACTION: 4,
+  APPLY_UPLOAD_CONTRACT_OPERATION: 5,
+  APPLY_EXECUTE_CONTRACT_OPERATION: 6,
+  APPLY_SET_SYSTEM_CALL_OPERATION: 7,
+  DB_PUT_OBJECT: 8,
+  DB_GET_OBJECT: 9,
+  DB_GET_NEXT_OBJECT: 10,
+  DB_GET_PREV_OBJECT: 11,
+  EXECUTE_CONTRACT: 12,
+  GET_ENTRY_POINT: 13,
+  GET_CONTRACT_ARGS_SIZE: 14,
+  GET_CONTRACT_ARGS: 15,
+  SET_CONTRACT_RETURN: 16,
+  EXIT_CONTRACT: 17,
+  GET_HEAD_INFO: 18,
+  HASH: 19,
+  RECOVER_PUBLIC_KEY: 20,
+  VERIFY_BLOCK_SIGNATURE: 21,
+  VERIFY_MERKLE_ROOT: 22,
+  GET_TRANSACTION_PAYER: 23,
+  GET_MAX_ACCOUNT_RESOURCES: 24,
+  GET_TRANSACTION_RESOURCE_LIMIT: 25,
+  GET_LAST_IRREVERSIBLE_BLOCK: 26,
+  GET_CALLER: 27,
+  REQUIRE_AUTHORITY: 28,
+  GET_TRANSACTION_SIGNATURE: 29,
+  GET_CONTRACT_ID: 30,
+  GET_HEAD_BLOCK_TIME: 31,
+  GET_ACCOUNT_NONCE: 32
+};
+
