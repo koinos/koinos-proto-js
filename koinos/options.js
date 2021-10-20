5c1b721cbcd25b6c74c3a92e529fadb5e87fd879
Koinos <67607993+koinos-ci@users.noreply.github.com>
2021-10-20 23:01:10 +0000

    Update for koinos-proto commit 9ef23b6

---
+++ koinos/options.js
@@ -0,0 +1,28 @@
+// source: koinos/options.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.koinos.bytes_type');
+
+/**
+ * @enum {number}
+ */
+proto.koinos.bytes_type = {
+  BASE64: 0,
+  BASE58: 1,
+  HEX: 2,
+  BLOCK_ID: 3,
+  TRANSACTION_ID: 4,
+  CONTRACT_ID: 5,
+  ADDRESS: 6
+};
+
